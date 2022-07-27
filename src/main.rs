208c7de5ea5abe9dc44497d40e6d92a1dda8f936
Tristan <LeoDog896@hotmail.com>
2022-07-27 00:05:58 -0400

    Reorganization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,88 @@
+use console::{Style, Term, Key};
+use dialoguer::{theme::ColorfulTheme, Input};
+use std::cmp::{max, min};
+use anyhow::Result;
+use treblecross::{solve, Game};
+
+fn print_game(game: &Game, position: usize) {
+    {
+        let state = game.state.clone();
+        let mut board = String::new();
+        for i in 0..game.size() {
+
+            let position = position;
+
+            let style = match position {
+                _ if position == i => Style::new().red(),
+                _ => Style::new().white(),
+            };
+
+            board.push_str(&format!(
+                "{: >4} ",
+                style.apply_to(match state.get(i as usize) {
+                    Some(0) | None => ".",
+                    Some(1) => "X",
+                    _ => unreachable!(),
+                })
+            ));
+        }
+        println!("{}", board);
+    }
+
+    {
+        let solved = solve(game);
+        let mut board = String::new();
+        for i in 0..game.size() {
+            board.push_str(&format!("{: >4} ", solved[i as usize]));
+        }
+
+        println!("{}", board);
+    }
+}
+
 fn main() {
-  
+    main_err().unwrap();
+}
+
+fn main_err() -> Result<()> {
+    let mut cursor_position: usize = 0;
+
+    let stdout = Term::stdout();
+
+    let length: usize = Input::with_theme(&ColorfulTheme::default())
+        .with_prompt("Length of board")
+        .default(5)
+        .interact_text()
+        .unwrap();
+
+    let mut game = Game::new(length);
+
+    loop {
+
+        let game_over = game.game_over();
+        
+        if game_over {
+            println!("Game over!");
+            break;
+        }
+
+        stdout.clear_screen()?;
+
+        print_game(&game, cursor_position);
+
+        if let Ok(key) = stdout.read_key() {
+            match key {
+                Key::Char('a') | Key::ArrowLeft => cursor_position = max(0, cursor_position - 1),
+                Key::Char('d') | Key::ArrowRight => cursor_position = min(length - 1, cursor_position + 1),
+                Key::Enter => {
+                    if game.can_play(cursor_position) {
+                        game.play(cursor_position);
+                    }
+                }
+                _ => continue
+            }
+        }
+    }
+
+    Ok(())
 }
---
+++ treblecross/src/lib.rs
@@ -0,0 +1,134 @@
+pub struct Game {
+    pub state: Vec<u16>
+}
+
+impl Game {
+    #[must_use]
+    pub fn new(size: usize) -> Self {
+        Self {
+            state: vec![0; size]
+        }
+    }
+
+    pub fn size(&self) -> usize {
+        self.state.len()
+    }
+
+    pub fn amount_played(&self) -> usize {
+        self.state.iter().filter(|&x| *x == 1).count()
+    }
+
+    pub fn can_play(&self, x: usize) -> bool {
+        self.state[x] == 0
+    }
+
+    /// The game is over when there are 3 or more consecutive 1s in the state.
+    pub fn game_over(&self) -> bool {
+        let mut consecutive = 0;
+        for i in 0..self.size() {
+            if self.state[i] == 1 {
+                consecutive += 1;
+            } else {
+                consecutive = 0;
+            }
+            if consecutive >= 3 {
+                return true;
+            }
+        }
+        false
+    }
+
+    pub fn is_winning_move(&self, x: usize) -> bool {
+        let mut consecutive = 0;
+        for i in 0..self.size() {
+            if i == x || self.state[i] == 1 {
+                consecutive += 1;
+            } else {
+                consecutive = 0;
+            }
+            if consecutive >= 3 {
+                return true;
+            }
+        }
+        false
+    }
+
+    pub fn play(&mut self, x: usize) {
+        self.state[x] = 1;
+    }
+}
+
+impl Clone for Game {
+    fn clone(&self) -> Self {
+        Self {
+            state: self.state.clone()
+        }
+    }
+}
+/// Solves a treblecross game using the negamax formula.
+/// The game is over when there are 3 filled cells (1s) in a row.
+pub fn solve(game: &Game) -> Vec<i16> {
+    game.state
+        .iter()
+        .enumerate()
+        .map(|(_, &x)| -> i16 {
+            
+            let x = x as usize;
+
+            if game.can_play(x) && game.is_winning_move(x) {
+                println!("{}", game.amount_played());
+                return ((game.size() + 1) as i16 - game.amount_played() as i16) / 2;
+            }
+
+            if game.can_play(x) {
+                let mut new_game = game.clone();
+                new_game.play(x);
+                return -(solve(&new_game).iter().max().unwrap() as &i16);
+            }
+
+            return -(game.size() as i16);
+        })
+        .collect::<Vec<i16>>()
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::Game;
+
+    #[test]
+    fn winning_move() {
+        let mut game = Game::new(5);
+
+        game.play(0);
+        game.play(1);
+
+        assert!(game.is_winning_move(2));
+
+        let mut game = Game::new(5);
+
+        game.play(1);
+        game.play(2);
+
+        assert!(game.is_winning_move(3));
+        assert!(game.is_winning_move(0));
+    }
+
+    #[test]
+    fn piece_set() {
+        let mut game = Game::new(5);
+        game.play(0);
+
+        assert!(game.state[0] == 1);
+    }
+
+    #[test]
+    fn game_over() {
+        let mut game = Game::new(5);
+        game.play(0);
+        game.play(1);
+        game.play(2);
+
+        assert!(game.game_over());
+    }
+}
